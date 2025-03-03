aca45fe6a5e73886d6d1ec4ec978f62c0ba1e304
ViPhong <viphongly2804@gmail.com>
2025-03-03 16:10:39 +0700

    Add user retrieval route and JWT decoding middleware

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
+const { decodeJwtMiddleware } = require("./middlewares/decodeToken");
 
 const indexRouter = require("./routes/index");
-const authRoute = require("./routes/auth");
+const userRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
 
 const app = express();
 
@@ -22,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/auth", authRoute);
+app.use("/users", decodeJwtMiddleware, userRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -35,9 +38,15 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  if (req.app.get("env") === "development") {
+    // print error to console in development
+    console.error(err);
+    res.status(err.status || 500).send(err.message);
+  } else {
+    // render the error page in production
+    res.status(err.status || 500);
+    res.render("error");
+  }
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", async function (req, res, next) {
+  try {
+    const userId = req.decoded.id;
+    const user = await User.findByPk(userId);
+    if (user) {
+      const userObj = user.toJSON();
+      delete userObj.password;
+      res.json(userObj);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
 });
-
-
 
 module.exports = router;
